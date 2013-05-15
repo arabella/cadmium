fb88f6bac00b2d6f21f81bd5d607b7b46ff27d28
Lina Ivanova <lina.greenaff@gmail.com>
2013-05-15 15:36:41 +0300

    nice try yesterday. now checked non versioned files

---
+++ Baloons-Pop-1/ScoreBoard.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace PoppingBaloons
+{
+    public class ScoreBoard
+    {
+        public  static void WriteScoresToFile(string filePath)
+        {
+            bool gameOver = true;
+            if (gameOver)
+            {
+                GetPlayerdata(); 
+
+                // TODO write to txt file
+            }
+        }
+
+        public  static ScoreEntry GetPlayerdata()
+        {
+            string data = string.Empty;
+           
+            //TODO get name and turns count
+
+            ScoreEntry entry = new ScoreEntry(data);
+            return entry;
+        }
+
+        internal static List<ScoreEntry> ReadScoresFromFile(string filePath)
+        {
+            var scores = new List<ScoreEntry>();
+
+            using (StreamReader myReader = new StreamReader(filePath))
+            {
+                string line = string.Empty;
+                while (!myReader.EndOfStream)
+                {
+                    line = myReader.ReadLine();
+
+                    try
+                    {
+                        scores.Add(new ScoreEntry(line));
+                    }
+                    catch (ArgumentException ex)
+                    {
+                        Console.WriteLine("Invalid score entry at line \"{0}\": {1}", line, ex);
+                    }
+                }
+            }
+            return SortScoresDescending(scores);
+        }
+
+        private static List<ScoreEntry> SortScoresDescending(List<ScoreEntry> scores)
+        {
+            scores = scores.OrderByDescending(s => s.Score).ToList();
+            int position = 1;
+            scores.ForEach(s => s.Position = position++);
+
+            return scores.ToList();
+        }
+    }
+}
---
+++ Baloons-Pop-1/ScoreEntry.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace PoppingBaloons
+{
+    public class ScoreEntry
+    {
+        public string PlayerName { get; set; }
+        public int Position { get; set; }
+        public int Score { get; set; }
+
+        public ScoreEntry(string data)
+        {
+            var entryData = data.Split(' ');
+
+            if (string.IsNullOrWhiteSpace(data) || entryData.Length < 2)
+            {
+                throw new ArgumentException("Invalid score entry", "data");
+            }
+
+            this.PlayerName = entryData[0];
+
+            int turns;
+            if (int.TryParse(entryData[1], out turns))
+            {
+                this.Score = turns;
+            }
+            else
+            {
+                throw new ArgumentException("Invalid score enrty", "data");
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}. {1}: {2}", this.Position, this.PlayerName, this.Score);
+        }
+    }
+}
