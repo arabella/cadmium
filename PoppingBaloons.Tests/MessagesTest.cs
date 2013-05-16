e0d895e23ab42eaefa862b102fa3407e6c4cefaf
ralikuman <http://windows.github.com>
2013-05-16 23:25:27 +0300

    add unit test project

---
+++ PoppingBaloons.Tests/MessagesTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace PoppingBaloons.Tests
+{
+    [TestClass]
+    public class MessagesTest
+    {
+        [TestMethod]
+        public void PrintMessagesUnknownCommand()
+        {
+            Messages.UnknownCommand();
+            using (StringWriter sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                Messages.UnknownCommand();
+                string expected = string.Format("Unknown Command!\r\n", Environment.NewLine);
+                Assert.AreEqual<string>(expected, sw.ToString());
+            }
+        }
+
+        [TestMethod]
+        public void PrintMessagesBye()
+        {
+            Messages.UnknownCommand();
+            using (StringWriter sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                Messages.Bye();
+                string expected = string.Format("Thanks for playing!!!\r\n", Environment.NewLine);
+                Assert.AreEqual<string>(expected, sw.ToString());
+            }
+        }
+    }
+}
