758db5becb6f3bbafacd7f7a13cda81089a29f39
ralikuman <http://windows.github.com>
2013-05-16 23:43:42 +0300

    add unit test about MessagesTest.cs except for method Win()

--- PoppingBaloons.Tests/MessagesTest.cs
+++ PoppingBaloons.Tests/MessagesTest.cs
@@ -8,9 +8,20 @@ namespace PoppingBaloons.Tests
     public class MessagesTest
     {
         [TestMethod]
+        public void PrintMessagesWelcome()
+        {
+            using (StringWriter sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                Messages.Welcome();
+                string expected = string.Format("Welcome to “Balloons Pops” game. Please try to pop the balloons.\r\n Use 'top' to view the top scoreboard, 'restart' to start a new game and 'exit' to quit the game.\r\n", Environment.NewLine);
+                Assert.AreEqual<string>(expected, sw.ToString());
+            }
+        }
+        
+        [TestMethod]
         public void PrintMessagesUnknownCommand()
         {
-            Messages.UnknownCommand();
             using (StringWriter sw = new StringWriter())
             {
                 Console.SetOut(sw);
@@ -23,7 +34,6 @@ namespace PoppingBaloons.Tests
         [TestMethod]
         public void PrintMessagesBye()
         {
-            Messages.UnknownCommand();
             using (StringWriter sw = new StringWriter())
             {
                 Console.SetOut(sw);
@@ -32,5 +42,53 @@ namespace PoppingBaloons.Tests
                 Assert.AreEqual<string>(expected, sw.ToString());
             }
         }
+
+        [TestMethod]
+        public void PrintMessagesInsertCommand()
+        {
+            using (StringWriter sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                Messages.InsertCommand();
+                string expected = string.Format("Insert row and column or other command\r\n", Environment.NewLine);
+                Assert.AreEqual<string>(expected, sw.ToString());
+            }
+        }
+
+        [TestMethod]
+        public void PrintMessagesInvalidMove()
+        {
+            using (StringWriter sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                Messages.InvalidMove();
+                string expected = string.Format("Invalid Move! Can not pop a baloon at that place!!\r\n", Environment.NewLine);
+                Assert.AreEqual<string>(expected, sw.ToString());
+            }
+        }
+
+        [TestMethod]
+        public void PrintMessagesOutOfRange()
+        {
+            using (StringWriter sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                Messages.OutOfRange();
+                string expected = string.Format("Indexes are too big for current board\r\n", Environment.NewLine);
+                Assert.AreEqual<string>(expected, sw.ToString());
+            }
+        }
+
+        //[TestMethod]
+        //public void PrintMessagesWin()
+        //{
+        //    using (StringWriter sw = new StringWriter())
+        //    {
+        //        Console.SetOut(sw);
+        //        Messages.Win(3);
+        //        string expected = string.Format("Congratulations!!You popped all the baloons in {0} moves!\r\n", Environment.NewLine);
+        //        Assert.AreEqual<string>(expected, sw.ToString());
+        //    }
+        //}
     }
 }
